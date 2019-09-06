c4db60f71490176f1dfe48217323ad3c7bab974a
Monika <monika.sanjoth@gmail.com>
2019-09-06 20:31:07 +0530

    android Activity Interface

---
+++ android/ActivityInterface/app/src/androidTest/java/com/example/sanjoth/activityinterface/DemoInterfaceTest.java
@@ -0,0 +1,13 @@
+package com.example.sanjoth.activityinterface;
+
+
+import android.widget.Toast;
+
+public class  DemoInterfaceTest {
+    void DemoInterfacemethod(){
+
+
+    }
+
+
+}
---
+++ android/ActivityInterface/app/src/main/java/com/example/sanjoth/activityinterface/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.sanjoth.activityinterface;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements DemoInterface {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DemoInterfacemethod();
+
+    }
+
+    @Override
+    public void DemoInterfacemethod() {
+        Toast t=Toast.makeText(getApplicationContext()," This message from interface",Toast.LENGTH_LONG);
+        t.show();
+
+
+    }
+
+
+
+}
